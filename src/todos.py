7e53d59d024d3ba7713e510959790882cbafc053
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2026-09-04 09:13:57 +0200

    data operations

--- src/todos.py
+++ src/todos.py
@@ -5,9 +5,14 @@ url = "https://jsonplaceholder.typicode.com/todos"
 response = requests.get(url)
 if response.status_code == 200:
     todos = response.json()
+    completed_todos = []
+    uncompleted_todos = []
     for todo in todos:
-        title = str(todo["title"])
-        if title.startswith('q') :
-            print(f'Completed ToDo: {todo["title"]} , Completed: {todo["completed"]}')
+        completed = todo['completed']
+        if completed:
+            completed_todos.append(todo)
+        else:
+            uncompleted_todos.append(todo)
+    print(completed_todos)
 else:
     print("no todos found")
