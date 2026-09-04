384db51f989406aa8011061cf91993a10f042783
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2026-09-04 09:08:25 +0200

    todo starts with

--- src/todos.py
+++ src/todos.py
@@ -6,8 +6,8 @@ response = requests.get(url)
 if response.status_code == 200:
     todos = response.json()
     for todo in todos:
-        completed = todo["completed"]
-        if completed:
+        title = str(todo["title"])
+        if title.startswith('q') :
             print(f'Completed ToDo: {todo["title"]} , Completed: {todo["completed"]}')
 else:
     print("no todos found")
